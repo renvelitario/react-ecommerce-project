a6c19e0a8036e618956a34bdafe1b829ac66565b
Ren Velitario <velitarioren@gmail.com>
2026-02-25 14:24:52 +0800

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router'
 import HomePage from './Pages/HomePage'
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<h1>Test Checkout</h1>} />
+    </Routes>
   )
 }
 
