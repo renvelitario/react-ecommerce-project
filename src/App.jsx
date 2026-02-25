255a2fcb44d3a8e5b2d9fe8eafa61b797f6db9f9
Ren Velitario <velitarioren@gmail.com>
2026-02-25 14:38:18 +0800

    Move CheckoutPage into React

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router'
 import HomePage from './Pages/HomePage'
 import './App.css'
+import CheckoutPage from './Pages/CheckoutPage'
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<h1>Test Checkout</h1>} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   )
 }
